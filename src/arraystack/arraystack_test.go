1d3c11112405417898f579ddc2cda047403b3c4a
bighope-lumiere <bighope.lumiere0916@gmail.com>
2020-06-12 22:45:41 +0900

    ArrayStack のテストコードを書いた

---
+++ src/arraystack/arraystack_test.go
@@ -0,0 +1,68 @@
+package arraystack
+import (
+    "testing"
+)
+
+func TestNew(t *testing.T) {
+    as := New()
+    if as.n != 0 {
+        t.Errorf("New ArrayStack has wrong size %v", as.n)
+    }
+}
+
+func TestAdd(t *testing.T) {
+    as := New()
+    as.Add(0, 1)
+    if as.n != 1 {
+        t.Errorf("ArrayStack has wrong size %v", as.n)
+    }
+    if as.array[0] != 1 {
+        t.Errorf("ArrayStack[0] has wrong value %v", as.array[0])
+    }
+    as.Add(0, -1)
+    if as.n != 1 {
+        t.Errorf("ArrayStack has wrong size %v", as.n)
+    }
+    if as.array[0] != -1 {
+        t.Errorf("ArrayStack[0] has wrong value %v", as.array[0])
+    }
+}
+
+func TestGet(t *testing.T) {
+    as := New()
+    for i := 0; i < 100; i++ {
+        as.Add(i, i)
+    }
+    for i := 0; i < 100; i++ {
+        if as.Get(i) != i {
+            t.Errorf("Get(%v) returns wrong value %v, Expected %v", i, as.Get(i), i)
+        }
+    }
+}
+
+func TestSet(t *testing.T) {
+    as := New()
+    for i := 0; i < 100; i++ {
+        as.Add(0, i)
+    }
+    as.Set(88, 199)
+    if as.Get(88) != 199 {
+        t.Errorf("Get(88) returns wrong value %v, Expected 199", as.Get(88))
+    }
+}
+
+func TestRemove(t *testing.T) {
+    as := New()
+    for i := 0; i < 100; i++ {
+        as.Add(i, i)
+    }
+    as.Set(88, 199)
+    as.Add(87, 133)
+    var test int
+    for i := 0; i < 80; i++ {
+        test = as.Remove(0)
+        if test != i {
+            t.Errorf("Remove(0) returns wrong value %v, Expected %v", test, i)
+        }
+    }
+}
